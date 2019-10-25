bae648af356c59a6adeb67c57a24f0ee71aa156d
august von trapp <augustvontrapp@gmail.com>
2019-10-24 22:44:39 -0700

    clean

--- build/index.test.js
+++ build/index.test.js
@@ -1,10 +1,5 @@
 "use strict";
 
-var _ = require(".");
-
-xtest('replaces blacklisted words with asterisks', function () {
-  expect((0, _.sensitiveWords)('The name of the NX with be Nintendo Switch', ['switch'])).toBe('The name of the NX with be Nintendo ****');
-});
-xtest('replaces multiple blacklisted words with asterisks', function () {
-  expect((0, _.sensitiveWords)('The name of the NX with be Nintendo Switch. The switch will be awesome.', ['switch'])).toBe('The name of the NX with be Nintendo ****. The **** will be awesome.');
+xtest('', function () {
+  expect(true).toBe(true);
 });
--- src/index.test.js
+++ src/index.test.js
@@ -1,15 +1,3 @@
-import { sensitiveWords } from '.'
-
-xtest('replaces blacklisted words with asterisks', () => {
-  expect(sensitiveWords(
-    'The name of the NX with be Nintendo Switch',
-   ['switch']
- )).toBe('The name of the NX with be Nintendo ****')
-})
-
-xtest('replaces multiple blacklisted words with asterisks', () => {
-  expect(sensitiveWords(
-    'The name of the NX with be Nintendo Switch. The switch will be awesome.',
-   ['switch']
- )).toBe('The name of the NX with be Nintendo ****. The **** will be awesome.')
+xtest('', () => {
+  expect(true).toBe(true)
 })
